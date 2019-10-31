33a9cfb87e61972ea52be0b76c21e1db9d66365b
Tanzim Ali <tanzim_rayhan@yahoo.com>
2019-10-31 17:50:50 -0400

    Added backend with express

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,6 +1,7 @@
-import React,{Component} from 'react'
-import {Link} from 'react-router-dom'
+import React,{Component} from 'react';
+import {Link} from 'react-router-dom';
 import '../App.css';
+import axios from 'axios';
 import {isAndroid,isMobile,isIOS, osName, osVersion,mobileModel} from 'react-device-detect';
 
 
@@ -35,6 +36,32 @@ class MainPage extends Component{
     var x="OS version : "+osVersion;
     return x;
   }
+  onChangeHandler=event=>{
+    this.setState({
+      selectedFile: event.target.files,
+      loaded: 0,
+    })
+    console.log(event.target.files)
+
+  }
+  constructor(props) {
+    super(props);
+      this.state = {
+        selectedFile: null
+      }
+  }
+  
+  onClickHandler = () => {
+    const data = new FormData() 
+    for(var x = 0; x<this.state.selectedFile.length; x++) {
+      data.append('file', this.state.selectedFile[x])
+    }
+    axios.post("http://localhost:8000/upload", data, { 
+      // receive two    parameter endpoint url ,form data
+  }).then(res=>{
+    console.log(res.statusText+"  File uploaded")
+  })
+  }
 
   render(){
       
@@ -43,7 +70,22 @@ class MainPage extends Component{
         document.title="CS355 PROJECT",
         <div className="App" style={{height:'100%'}}>
           <header className="App-header" >
-            
+            <div className="Container">
+              <div className="Row">
+                <div className="offset-md-3 col-md-6">
+                  <div className="form-group files">
+                    <label>Upload your file</label>
+                    <input type="file" name="file" multiple onChange={this.onChangeHandler}/>
+                    <div>
+                      <br/>
+                    <button type="button" class="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button> 
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br/>
+
             <p>Welcome to <code>Search and Reporting Application</code> </p>
             <p>
               <code> {this.checkIfMobile()} </code>
